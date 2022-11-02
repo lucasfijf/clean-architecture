f0130efe84a5a215ddc27ed646251dbacee509ce
Lucas Rangel <lucasfijf@gmail.com>
2022-11-01 22:50:26 -0300

    feat: creating UserRepository with insert_user and tests

---
+++ src/infra/repositories/user_repository_test.py
@@ -0,0 +1,28 @@
+from faker import Faker
+from src.infra.config import DBConnectionHandler
+from .user_repository import UserRepository
+
+faker = Faker()
+user_repository = UserRepository()
+db_connection_handler = DBConnectionHandler()
+
+
+def test_insert_user():
+    """Should insert user"""
+
+    # Arrange
+    name = faker.name()
+    password = faker.word()
+    engine = db_connection_handler.get_engine()
+
+    # Act
+    new_user = user_repository.insert_user(name, password)
+    query_user = engine.execute(
+        f"SELECT * FROM users WHERE id={new_user.id}"
+    ).fetchone()
+    engine.execute(f"DELETE FROM users WHERE id={new_user.id}")
+
+    # Assert
+    assert new_user.id == query_user.id
+    assert new_user.name == query_user.name
+    assert new_user.password == query_user.password
