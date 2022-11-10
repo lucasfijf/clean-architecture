9b4f87b37571d1d015453278412558a87a43b096
Lucas Rangel <lucasfijf@gmail.com>
2022-11-10 12:49:16 -0300

    feat: implementing register user route with flask

---
+++ src/main/adapter/api_adapter.py
@@ -0,0 +1,24 @@
+from typing import Type
+from sqlalchemy.exc import IntegrityError
+from src.main.interfaces import RouteInterface as Route
+from src.presenters.helpers import HttpRequest, HttpResponse
+from src.presenters.errors import HttpErrors
+
+
+def flask_adapter(request: any, api_route: Type[Route]) -> any:
+    """Adapter pattern to Flask
+    :param - flask request
+           - api_route: composite routes
+    """
+
+    http_request = HttpRequest(body=request.json)
+
+    try:
+        response = api_route.handle(http_request)
+    except IntegrityError:
+        http_error = HttpErrors.error_409()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
+
+    return response
--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -1,10 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.main.composer import register_user_composer
+from src.main.adapter import flask_adapter
 
 api_routes_bp = Blueprint("api_routes", __name__)
 
 
-@api_routes_bp.route("/api", methods=["GET"])
-def something():
-    """teste"""
+@api_routes_bp.route("/api/users", methods=["POST"])
+def register_user():
+    """Register user route"""
 
-    return jsonify({"Programador": "lucas"})
+    message = None
+    response = flask_adapter(request=request, api_route=register_user_composer())
+    if response.status_code < 300:
+        message = {
+            "Type": "users",
+            "id": response.body.id,
+            "attributes": {"name": response.body.name},
+        }
+
+        return jsonify({"data": message}), response.status_code
+
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )
